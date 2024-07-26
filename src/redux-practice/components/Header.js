43ea550cb6652345b0a36d45a7dfbfabb638cbed
dazzlingtea <kitsnr08@gmail.com>
2024-07-26 11:42:12 +0900

    # 리덕스 툴킷 슬라이스 모듈화

--- src/redux-practice/components/Header.js
+++ src/redux-practice/components/Header.js
@@ -1,6 +1,7 @@
 import classes from './Header.module.css';
 import {useDispatch, useSelector} from "react-redux";
-import {authActions} from "../store";
+import {authActions} from "../store/authSlice";
+
 
 const Header = () => {
 
---
+++ src/redux-practice/store/authSlice.js
@@ -0,0 +1,24 @@
+
+// 인증 관련 초기 상태값
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialAuthState = {
+  isLoggedIn: false
+};
+
+// 인증 관련 슬라이스 추가
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    }
+  }
+});
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
