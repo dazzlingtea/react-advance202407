61d6fd3217942295030ece4a113fc2228c211dae
dazzlingtea <kitsnr08@gmail.com>
2024-07-26 10:55:56 +0900

    # 리덕스 툴킷 컴포넌트 세팅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import ReduxCounter from "./redux-practice/components/ReduxCounter";
+import Auth from "./redux-practice/components/Auth";
+import Header from "./redux-practice/components/Header";
+import UserProfile from "./redux-practice/components/UserProfile";
 
 const App = () => {
 
   return (
     <>
+      <Header />
+      <Auth />
+      <UserProfile />
       <ReduxCounter />
     </>
   );
